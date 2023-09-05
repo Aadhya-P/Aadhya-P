260bddb35b9f4abf245a3e048f6e18dbe2cf0ed0
Aadhya-P <143151441+Aadhya-P@users.noreply.github.com>
2023-09-05 07:09:49 +0000

    score_detector.py

---
+++ my_projects01/score_detector.py
@@ -0,0 +1,120 @@
+scoreM = int(input("What is your maths score:"))
+scoreE = int(input("What is your english score:"))
+scoreSc = int(input("What is your science score: "))
+scoreSo = int(input("What is your social studies score:"))
+scoreT = int(input("What is your Telugu score:"))
+scoreH = int(input("What is your Hindi score:"))
+
+if scoreM >= 20 :
+    print(scoreM)
+    print("your maths grade is Grade A+")
+elif scoreM >= 18 :
+    print(scoreM)
+    print("your maths grade is Grade A")
+elif scoreM >= 15 :
+    print(scoreM)
+    print("your maths grade is Grade B")
+elif scoreM >= 12 :
+    print(scoreM)
+    print("your maths grade is Grade C")
+elif scoreM >= 10 :
+    print(scoreM)
+    print("your maths grade is Grade D")
+else :
+    print(scoreM)
+    print("your maths grade is Grade F")
+
+if scoreE >= 20 :
+    print(scoreE)
+    print("your english grade is Grade A+")
+elif scoreE >= 18 :
+    print(scoreE)
+    print("Your english grade is Grade A")
+elif scoreE >= 15 :
+    print(scoreE)
+    print("your english grade is Grade B")
+elif scoreE >= 12 :
+    print(scoreE)
+    print("your english grade is Grade C")
+elif scoreE >= 10 :
+    print(scoreE)
+    print("your english grade is Grade D")
+else :
+    print(scoreM)
+    print("your english grade is Grade F")
+
+if scoreSc >= 20 :
+    print(scoreSc)
+    print("your Science grade is Grade A+")
+elif scoreM >= 18 :
+    print(scoreSc)
+    print("your Science grade is Grade A")
+elif scoreM >= 15 :
+    print(scoreSc)
+    print("your Science grade is Grade B")
+elif scoreM >= 12 :
+    print(scoreSc)
+    print("your Science grade is Grade C")
+elif scoreM >= 10 :
+    print(scoreSc)
+    print("your Science grade is Grade D")
+else :
+    print(scoreSc)
+    print("your Science grade is Grade F")
+
+if scoreSo >= 20 :
+    print(scoreSo)
+    print("your Social grade is Grade A+")
+elif scoreSo >= 18 :
+    print(scoreSo)
+    print("Your Social grade is Grade A")
+elif scoreSo >= 15 :
+    print(scoreSo)
+    print("your Social grade is Grade B")
+elif scoreE >= 12 :
+    print(scoreE)
+    print("your Social grade is Grade C")
+elif scoreSo >= 10 :
+    print(scoreSo)
+    print("your Social grade is Grade D")
+else :
+    print(scoreM)
+    print("your Social grade is Grade F")
+
+if scoreT >= 20 :
+    print(scoreT)
+    print("your telugu grade is Grade A+")
+elif scoreT >= 18 :
+    print(scoreT)
+    print("your telugu grade is Grade A")
+elif scoreT >= 15 :
+    print(scoreT)
+    print("your telugu grade is Grade B")
+elif scoreT >= 12 :
+    print(scoreT)
+    print("your telugu grade is Grade C")
+elif scoreT >= 10 :
+    print(scoreT)
+    print("your telugu grade is Grade D")
+else :
+    print(scoreT)
+    print("your telugu grade is Grade F")
+
+if scoreH >= 20 :
+    print(scoreH)
+    print("your hindi grade is Grade A+")
+elif scoreH >= 18 :
+    print(scoreH)
+    print("Your hindi grade is Grade A")
+elif scoreH >= 15 :
+    print(scoreH)
+    print("your hindi grade is Grade B")
+elif scoreH >= 12 :
+    print(scoreH)
+    print("your hindi grade is Grade C")
+elif scoreH >= 10 :
+    print(scoreH)
+    print("your hindi grade is Grade D")
+else :
+    print(scoreH)
+    print("your hindi grade is Grade F")
